a791f93124749975775478082d904a0f1b85947b
Wu Hanfeng <dankejun0.0@gmail.com>
2025-07-04 10:29:24 +0800

    feat: 用户crud

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ import SvgIcon from './icons'; // icon
 import './permission'; // permission control
 import vPermission from './directive/permission/index'; // permission control
 import { checkEnableLogs } from './utils/error-log'; // error log
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
 
 const app = createApp(App);
 setupStore(app);
@@ -16,5 +17,8 @@ app.use(router);
 app.component('svg-icon', SvgIcon);
 app.directive('permission', vPermission);
 checkEnableLogs(app);
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
 
 app.mount('#app');
