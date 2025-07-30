96ea18f50de77ef2a7c72866b2c829d61200a4f7
Wu Hanfeng <dankejun0.0@gmail.com>
2025-07-30 09:49:43 +0800

    feat: 菜单管理

--- src/views/system/sysRole.vue
+++ src/views/system/sysRole.vue
@@ -50,8 +50,29 @@
         <el-button type="danger" size="small" @click="deleteById(scope.row)">
           删除
         </el-button>
+        <el-button type="warning" size="small" @click="showAssignMenu(scope.row)">
+          分配菜单
+        </el-button>
       </el-table-column>
     </el-table>
+
+    <el-dialog v-model="dialogMenuVisible" title="分配菜单" width="40%">
+      <el-form label-width="80px">
+        <el-tree
+            :data="sysMenuTreeList"
+            ref="tree"
+            show-checkbox
+            default-expand-all
+            :check-on-click-node="true"
+            node-key="id"
+            :props="defaultProps"
+        />
+        <el-form-item>
+          <el-button type="primary" @click="doAssign">提交</el-button>
+          <el-button @click="dialogMenuVisible = false">取消</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
 
     <!--分页条-->
     <el-pagination
@@ -69,8 +90,70 @@
 
 <script setup>
 import { ref } from 'vue';
-import { DeleteSysRoleById, GetSysRoleListByPage, SaveSysRole, UpdateSysRole } from '@/api/sysRole';
+import {
+  DeleteSysRoleById,
+  DoAssignMenuIdToSysRole,
+  GetSysRoleListByPage,
+  GetSysRoleMenuIds,
+  SaveSysRole,
+  UpdateSysRole
+} from '@/api/sysRole';
 import { onMounted } from 'vue';
+
+const doAssign = async () => {
+  const checkedNodes = tree.value.getCheckedNodes() ; // 获取选中的节点
+  const checkedNodesIds = checkedNodes.map(node => {  // 获取选中的节点的id
+    return {
+      id: node.id,
+      isHalf: 0
+    }
+  })
+
+  // 获取半选中的节点数据，当一个节点的子节点被部分选中时，该节点会呈现出半选中的状态
+  const halfCheckedNodes = tree.value.getHalfCheckedNodes() ;
+  const halfCheckedNodesIds = halfCheckedNodes.map(node => {   // 获取半选中节点的id
+    return {
+      id: node.id,
+      isHalf: 1
+    }
+  })
+
+  // 将选中的节点id和半选中的节点的id进行合并
+  const menuIds = [...checkedNodesIds , ...halfCheckedNodesIds]
+  console.log(menuIds);
+
+  // 构建请求数据
+  const assignMenuDto = {
+    roleId: roleId,
+    menuIdList: menuIds
+  }
+
+  // 发送请求
+  await DoAssignMenuIdToSysRole(assignMenuDto) ;
+  ElMessage.success('操作成功')
+  dialogMenuVisible.value = false
+
+}
+
+const defaultProps = {
+  children: 'children',
+  label: 'title',
+}
+const dialogMenuVisible = ref(false)
+const sysMenuTreeList = ref([])
+
+// 树对象变量
+const tree = ref()
+
+// 默认选中的菜单数据集合
+let roleId = ref()
+const showAssignMenu = async row => {
+  dialogMenuVisible.value = true
+  roleId = row.id
+  const { data } = await GetSysRoleMenuIds(row.id)   // 请求后端地址获取所有的菜单数据，以及当前角色所对应的菜单数据
+  sysMenuTreeList.value = data.sysMenuList
+  tree.value.setCheckedKeys(data.roleMenuIds)   // 进行数据回显
+}
 
 // 分页条总记录数
 const total = ref(0);
